0b78c0cdfcee38d85de5030ed10cf663d5ddb24e
Lichao Huang <lhuang38@rrc.ca>
2025-10-16 01:03:14 -0500

    validatename

--- script.js
+++ script.js
@@ -5,15 +5,30 @@ Assignment 4
 */
 
 /* ------------------------ Name Validation ------------------------ */
+const nameInput = document.getElementById("name");
+const nameError = document.getElementById("ticketsError");
 
+function validateName() {
+    const value = nameInput.value.trim();
+    if (value === "") {
+        nameError.textContent = "Name cannot be empty.";
+        return false;
+    }
+    nameError.textContent = "";
+    return true;
+}
 
 /* ------------------------ Profession Validation ------------------------ */
-
+const professionInputs = document.getElementsByName("profession");
+const professionError = document.getElementById("professionError");
 
 /* ------------------------ Talent Validation ------------------------ */
-
+const talentInputs = document.getElementsByName("talent");
+const talentError = document.getElementById("talentError");
 
 /* ------------------------ City Validation ------------------------ */
-
+const citySelect = document.getElementById("eventType");
+const cityError = document.getElementById("cityError");
 
 /* ------------------------ Button ------------------------ */
+const submitButton = document.getElementById("button");
